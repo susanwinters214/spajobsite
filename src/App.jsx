f0cfb1704eb52710e44910c8a3c90c19c3560f21
Susan Winters <susanwinters214@gmail.com>
2024-05-10 10:49:28 -0500

    setting up React18 project - jobBoard

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import HomeCards from "./components/HomeCards";
+import JobListings from "./components/JobListings";
+
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Hero
+        title="Test Big Title"
+        subtitle="Just a little subtitle for this area"
+      />
+      <HomeCards />
+      <JobListings />
+      <section className="m-auto max-w-lg my-10 px-6">
+        <a
+          href="jobs.html"
+          className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+        >
+          View All Jobs
+        </a>
+      </section>
+    </>
+  );
+};
+
+export default App;
